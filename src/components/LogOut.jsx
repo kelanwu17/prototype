34d872441511ea1535d9ce60f1c556af5b7cf7d3
Kelan Wu <90233632+kelanwu17@users.noreply.github.com>
2023-04-07 14:35:10 -0500

    Add files via upload

---
+++ src/components/LogOut.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {auth} from '../firebase'
+
+const style = {
+    button: `btn btn-ghost normal-case text-xl`
+}
+
+
+const LogOut = () => {
+const signOut = () => {
+    signOut(auth)
+}
+
+  return (
+    <div className="containerWrap flex justify-between">
+      <button onClick={() => auth.signOut()} className={style.button}>
+              Logout
+      </button>
+    </div>
+    
+  )
+}
+
+export default LogOut
